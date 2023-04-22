564488a6a19e728b0776f73ac3ab8c31b7830317
adeka stanley <adekastanley2@gmail.com>
2023-04-22 21:53:33 +0100

    test upload - animated cards

--- main.js
+++ main.js
@@ -1,197 +1,167 @@
+const hero = gsap.timeline();
+hero.from(".hero-section", {
+  opacity: 0.5,
+  ease: 1,
+  delay: 2,
+  duration: 1,
+  ease: 1,
+});
 
+hero.from(".hero-text-container", {
+  opacity: 0,
+  duration: 1,
+  ease: 1,
+  y: 100,
+});
+hero.from("#mainNavigation", {
+  opacity: 0,
+  duration: 1,
+  ease: 1,
+  y: -100,
+});
+hero.from(".nav-item", {
+  opacity: 0,
+  duration: 0.5,
+  ease: 1,
+  stagger: 0.5,
+  y: -100,
+});
+hero.to(".hero-text-container", {
+  // opacity: 0,
+  duration: 0.5,
+  ease: 1,
 
-const hero = gsap.timeline()
-hero.from('.hero-section', {
-    opacity: 0.5,
-    ease: 1,
-    delay: 2,
-    duration: 1,
-    ease: 1
-})
+  y: -100,
+});
+hero.from(".hero-text-container .tagline", {
+  opacity: 0,
+  duration: 0.5,
+  ease: 1,
 
-hero.from('.hero-text-container', {
-    opacity: 0,
-    duration: 1,
-    ease: 1,
-    y:100
-})
-hero.from('#mainNavigation', {
-    opacity: 0,
-    duration: 1,
-    ease: 1,
-    y:-100
-})
-hero.from('.nav-item', {
-    opacity: 0,
-    duration: .5,
-    ease: 1,
-    stagger: 0.5,
-    y:-100
-})
-hero.to('.hero-text-container', {
-    // opacity: 0,
-    duration: .5,
-    ease: 1,
+  y: 20,
+});
+hero.to(".scroll-container", {
+  opacity: 0.5,
+  duration: 1.5,
+  ease: 1,
+  y: -30,
+  repeat: -1,
+});
 
-    y:-100
-})
-hero.from('.hero-text-container .tagline', {
-    opacity: 0,
-    duration: .5,
-    ease: 1,
-
-    y: 20,
-
-})
-hero.to('.scroll-container', {
-    opacity: 0.5,
-    duration: 1.5,
-    ease: 1,
-    y: -30,
-    repeat: -1
-
-})
-
-
-const toggler = document.querySelector('.navbar-toggler');
+const toggler = document.querySelector(".navbar-toggler");
 let toggle = false;
-toggler.addEventListener('click', ()=> {
-    if (toggle === false){
-        gsap.to('main', {
-            opacity: 0,
-            duration: 1.5,
-            ease: 1,
-        })
-        toggle = true
-    } else if (toggle === true){
-        gsap.to('main', {
-            opacity: 1,
-            duration: 1.5,
-            ease: 1,
-        })
-        toggle = false
+toggler.addEventListener("click", () => {
+  if (toggle === false) {
+    gsap.to("main", {
+      opacity: 0,
+      duration: 1.5,
+      ease: 1,
+    });
+    toggle = true;
+  } else if (toggle === true) {
+    gsap.to("main", {
+      opacity: 1,
+      duration: 1.5,
+      ease: 1,
+    });
+    toggle = false;
+  }
+});
+
+const mobile = gsap.matchMedia();
+gsap.registerPlugin(ScrollTrigger);
+const sec1 = gsap.timeline();
+const sec2 = gsap.timeline();
+
+mobile.add("(max-width: 999px)", () => {
+  sec1.from(".section-1", {
+    opacity: 0,
+    duration: 1,
+    ease: 1,
+    x: -40,
+    scrollTrigger: {
+      trigger: ".section-1",
+      start: "top 80%",
+      bottom: "bottom 90%",
+      scrub: true,
+    },
+  });
+  sec2.from('.work', {
+    y: 100,
+    ease: 1,
+    opacity: 0,
+    // stagger: 0.5,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".section-2",
+        start: "top 90%",
+        end: "bottom 100%",
+        scrub: true,
     }
-})
+  })
+});
+const sec1Text = gsap.timeline();
+sec1Text.from(".text-container h2", {
+  opacity: 0,
+  duration: 1,
+  // delay: 1,
+  ease: 1,
+  y: 200,
+  scrollTrigger: {
+    trigger: ".section-1",
+    start: "top center",
+    end: "bottom bottom",
+    scrub: true,
+  },
+});
+sec1Text.from(".text-container p", {
+  opacity: 0,
+  duration: 1,
+  // delay: 1,
+  ease: 1,
+  y: 200,
+  scrollTrigger: {
+    trigger: ".section-1",
+    start: "top center",
+    end: "bottom bottom",
+    scrub: true,
+  },
+});
 
+// pc
+const pc = gsap.matchMedia();
+gsap.registerPlugin(ScrollTrigger);
 
+pc.add("(min-width: 100px)", () => {
+  sec1.from(".section-1", {
+    opacity: 0,
+    duration: 1,
+    ease: 1,
+    y: 100,
+    scrollTrigger: {
+      trigger: ".section-1",
+      start: "top 80%",
+      end: "bottom 90%",
+      scrub: true,
+    },
+  });
+  
+  sec2.from('.section-2', {
 
-// // carousel 
-// const carousel = document.querySelector(".carousel"),
-// firstImg = carousel.querySelectorAll("img")[0],
-// arrowIcons = document.querySelectorAll(".wrapper i");
+  })
 
-// let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
+  sec2.from('.work', {
+    y: 100,
+    ease: 1,
+    opacity: 0,
+    stagger: 0.5,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".section-2",
+        start: "top 90%",
+        end: "bottom ",
+        scrub: true,
+    }
+  })
 
-// const showHideIcons = () => {
-//     // showing and hiding prev/next icon according to carousel scroll left value
-//     let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
-//     arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
-//     arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
-// }
-
-// arrowIcons.forEach(icon => {
-//     icon.addEventListener("click", () => {
-//         let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
-//         // if clicked icon is left, reduce width value from the carousel scroll left else add to it
-//         carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
-//         setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
-//     });
-// });
-
-// const autoSlide = () => {
-//     // if there is no image left to scroll then return from here
-//     if(carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
-
-//     positionDiff = Math.abs(positionDiff); // making positionDiff value to positive
-//     let firstImgWidth = firstImg.clientWidth + 14;
-//     // getting difference value that needs to add or reduce from carousel left to take middle img center
-//     let valDifference = firstImgWidth - positionDiff;
-
-//     if(carousel.scrollLeft > prevScrollLeft) { // if user is scrolling to the right
-//         return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
-//     }
-//     // if user is scrolling to the left
-//     carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
-// }
-
-// const dragStart = (e) => {
-//     // updatating global variables value on mouse down event
-//     isDragStart = true;
-//     prevPageX = e.pageX || e.touches[0].pageX;
-//     prevScrollLeft = carousel.scrollLeft;
-// }
-
-// const dragging = (e) => {
-//     // scrolling images/carousel to left according to mouse pointer
-//     if(!isDragStart) return;
-//     e.preventDefault();
-//     isDragging = true;
-//     carousel.classList.add("dragging");
-//     positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
-//     carousel.scrollLeft = prevScrollLeft - positionDiff;
-//     showHideIcons();
-// }
-
-// const dragStop = () => {
-//     isDragStart = false;
-//     carousel.classList.remove("dragging");
-
-//     if(!isDragging) return;
-//     isDragging = false;
-//     autoSlide();
-// }
-
-// carousel.addEventListener("mousedown", dragStart);
-// carousel.addEventListener("touchstart", dragStart);
-
-// document.addEventListener("mousemove", dragging);
-// carousel.addEventListener("touchmove", dragging);
-
-// document.addEventListener("mouseup", dragStop);
-// carousel.addEventListener("touchend", dragStop);
-
-
-
-(function($) {
-
-	"use strict";
-
-	var fullHeight = function() {
-
-		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
-			$('.js-fullheight').css('height', $(window).height());
-		});
-
-	};
-	fullHeight();
-
-	var carousel = function() {
-		$('.featured-carousel').owlCarousel({
-	    loop:true,
-	    autoplay: true,
-	    margin:30,
-	    animateOut: 'fadeOut',
-	    animateIn: 'fadeIn',
-	    nav:true,
-	    dots: true,
-	    autoplayHoverPause: false,
-	    items: 1,
-	    navText : ["<span class='ion-ios-arrow-back'></span>","<span class='ion-ios-arrow-forward'></span>"],
-	    responsive:{
-	      0:{
-	        items:1
-	      },
-	      600:{
-	        items:2
-	      },
-	      1000:{
-	        items:3
-	      }
-	    }
-		});
-
-	};
-	carousel();
-
-})(jQuery);
+});
