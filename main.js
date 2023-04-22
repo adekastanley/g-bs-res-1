bc691835e3b17ffe2dd28e7f7fbc8459e99430de
adeka stanley <adekastanley2@gmail.com>
2023-04-22 14:00:16 +0100

    done hero animations

--- main.js
+++ main.js
@@ -3,9 +3,12 @@
 const hero = gsap.timeline()
 hero.from('.hero-section', {
     opacity: 0.5,
+    ease: 1,
+    delay: 1,
     duration: 1,
     ease: 1
 })
+
 hero.from('.hero-text-container', {
     opacity: 0,
     duration: 1,
@@ -17,4 +20,26 @@ hero.from('#mainNavigation', {
     duration: 1,
     ease: 1,
     y:-100
+})
+hero.from('.nav-item', {
+    opacity: 0,
+    duration: .5,
+    ease: 1,
+    stagger: 0.5,
+    y:-100
+})
+hero.to('.hero-text-container', {
+    // opacity: 0,
+    duration: .5,
+    ease: 1,
+
+    y:-100
+})
+hero.from('.hero-text-container .tagline', {
+    opacity: 0,
+    duration: .5,
+    ease: 1,
+
+    y: 20,
+
 })
